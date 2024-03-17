97a89f68bdf07abb70b2b9e36ab015bfd5526bac
Hibjul Ahmed <hibjulahmed@gmail.com>
2024-03-17 22:04:58 +0530

    added types and enums

---
+++ src/lib/enum.ts
@@ -0,0 +1,14 @@
+export enum DeviceType {
+  MOBILE = 'mobile',
+  TABLET = 'tablet',
+  LAPTOP = 'laptop',
+}
+
+export enum BrowserType {
+  CHROME = 'Chrome',
+  FIREFOX = 'Firefox',
+  SAFARI = 'Safari',
+  EDGE = 'Edge',
+  OPERA = 'Opera',
+  OTHER = 'Other',
+}
